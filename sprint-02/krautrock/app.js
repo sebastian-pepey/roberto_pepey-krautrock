25c722cfa75c4634d0e3f025820d848aee98d269
sebastian-pepey <robertopepey@gmail.com>
2022-07-28 09:17:02 -0300

    cambio estructura de archivos. Agregado division por sprints

---
+++ sprint-02/krautrock/app.js
@@ -0,0 +1,10 @@
+const express =require('express');
+const path = require('path');
+
+const app=express();
+
+app.listen(3000,()=>{console.log('servidor corriendo')});
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/index.html'));
+});
