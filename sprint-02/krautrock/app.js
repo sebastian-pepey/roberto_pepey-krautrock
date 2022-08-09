1b62c0a9e304a692e5a904452e2a75ad75280d26
sebastian-pepey <robertopepey@gmail.com>
2022-08-09 00:01:33 -0300

    Agregado index.html

--- sprint-02/krautrock/app.js
+++ sprint-02/krautrock/app.js
@@ -5,6 +5,8 @@ const app=express();
 
 app.listen(3000,()=>{console.log('servidor corriendo')});
 
+app.use(express.static(path.join(__dirname,'/public')));
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/index.html'));
 });
