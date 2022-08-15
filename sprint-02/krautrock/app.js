8592f64af54e3942f0e7d68d58846235b6d6249e
sebastian-pepey <robertopepey@gmail.com>
2022-08-15 17:58:12 -0300

    Actualización proyecto

--- sprint-02/krautrock/app.js
+++ sprint-02/krautrock/app.js
@@ -10,3 +10,19 @@ app.use(express.static(path.join(__dirname,'/public')));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/index.html'));
 });
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'));
+});
+
+app.get('/product',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/product.html'));
+});
+
+app.get('/clases',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/clases.html'));
+});
+
+app.post('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register-success.html'));
+});
